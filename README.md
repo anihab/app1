efe509eaf5c1ed69fedc4b1a4a75431541d52b94
Anisa Habib <anisa.s.habib@gmail.com>
2023-02-15 15:00:25 -0700

    added all required text views, edit texts, buttons, and image view. beginning to work on implementations.

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # app1
+
+A simple Android app to accomplish the following:
+
+1. Accept the user's first, middle, and last names in separate EditTexts.
+
+2. Ask the user to take a profile picture using the phone camera and show a thumbnail on the app.
+
+3. When the user hits a submit button, take them to a second page. 
+   This page says "First name last name is logged in!", where "First name" and "last name" are the 
+   names entered on the previous page.
+
+Most importantly, each page of the app (an Activity) should have its lifecycle managed. 
+Strategically use OnSaveInstanceState to back up UI details into a bundle, and restore 
+backups in either OnCreate or OnRestoreInstanceState. We will test your app by enabling auto-rotate, 
+but will also test pausing and resuming the app. Do all your testing for the Pixel 6 on API 33.
--- app/src/main/java/com/example/app1/MainActivity.kt
+++ app/src/main/java/com/example/app1/MainActivity.kt
@@ -1,17 +1,69 @@
 package com.example.app1
 
+import android.content.ActivityNotFoundException
 import androidx.appcompat.app.AppCompatActivity
+import android.widget.TextView
+import android.widget.EditText
+import android.widget.ImageView
+import android.graphics.Bitmap
+import android.widget.Button
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.provider.MediaStore
+import androidx.activity.result.contract.ActivityResultContracts
 
-// Accept the user's first, middle, and last names in separate EditTexts.
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    // strings
+    private var mFirstName: String? = null
+    private var mMiddleName: String? = null
+    private var mLastName: String? = null
 
-// Ask the user to take a profile picture using the phone camera and show a thumbnail on the app.
+    // UI elements
+    private var mTvFirstName: TextView? = null
+    private var mTvMiddleName: TextView? = null
+    private var mTvLastName: TextView? = null
+    private var mButtonSubmit: Button? = null
+    private var mButtonCamera: Button? = null
+    private var mIvProfilePicture: ImageView? = null
 
-// When the user hits a submit button, take them to a second page. This page says "First name last name is logged in!", where "First name" and "last name" are the names entered on the previous page.
-
-class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // initialize buttons
+        mButtonSubmit = findViewById<View>(R.id.button_submit) as Button
+        mButtonCamera = findViewById<View>(R.id.button_camera) as Button
+
+        mButtonSubmit!!.setOnClickListener(this)
+        mButtonCamera!!.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        when (view.id) {
+            // when the camera button is clicked
+            R.id.button_camera -> {
+                // open the camera
+                val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                try {
+                    cameraActivity.launch(cameraIntent)
+                } catch (ex: ActivityNotFoundException) {
+                    // do nothing
+                }
+            }
+
+            // when the submit button is clicked
+            R.id.button_submit -> {
+
+            }
+        }
+    }
+
+    private var cameraActivity = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == RESULT_OK) {
+            mIvProfilePicture = findViewById<View>(R.id.iv_pfp) as ImageView
+            val previewImage =  result.data!!.getParcelableExtra<Bitmap>("data")
+            mIvProfilePicture!!.setImageBitmap(previewImage)
+        }
     }
 }
